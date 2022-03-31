89dc2d12c02ed0435d78adb162232bd8cd264f5b
Moh. Al-Fadhel D. Cali <mdcali@up.edu.ph>
2022-03-31 11:10:18 +0800

    transfer from home to admin_creds

--- admin_access/models.py
+++ admin_access/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class admin_creds(models.Model):
+  username = models.CharField(max_length=50)
+  password = models.CharField(max_length=50)
+  fullname = models.CharField(max_length=100)
+  position = models.CharField(max_length=100)
+  email = models.CharField(max_length=100)
+
+  def __str__(self) -> str:
+    return 'Full name: %s, Position: %s, Email: %s' % self.fullname, self.position, self.email
--- home/models.py
+++ home/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class admin_creds(models.Model):
-  username = models.CharField(max_length=50)
-  password = models.CharField(max_length=50)
-  fullname = models.CharField(max_length=100)
-  position = models.CharField(max_length=100)
-  email = models.CharField(max_length=100)
-
-  def __str__(self) -> str:
-    return 'Full name: %s, Position: %s, Email: %s' % self.fullname, self.position, self.email
+# Create your models here.
