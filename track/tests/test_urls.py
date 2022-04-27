1a323b7766a505a3fa6f8e594037cd8994226e6c
Moh. Al-Fadhel D. Cali <mdcali@up.edu.ph>
2022-04-27 10:52:38 +0800

    track unit tests

--- track/tests/test_urls.py
+++ track/tests/test_urls.py
@@ -1,3 +1,15 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from track.views import results, track
+
+class TestUrls(SimpleTestCase):
+
+  def test_track_url_resolves(self):
+    url = reverse('track')
+    print(resolve(url))
+    self.assertEquals(resolve(url).func, track)
+
+  def test_results_url_resolves(self):
+    url = reverse('results')
+    print(resolve(url))
+    self.assertEquals(resolve(url).func, results)
