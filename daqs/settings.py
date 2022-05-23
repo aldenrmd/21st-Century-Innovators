eefeae3e1d665a47c57e3fec52f7771ddfc31bf4
Moh. Al-Fadhel D. Cali <mdcali@up.edu.ph>
2022-05-23 10:56:48 +0800

    Update settings.py

--- daqs/settings.py
+++ daqs/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '3306',
         'USER': 'root',
         'PASSWORD': '',
     }
