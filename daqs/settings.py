a0beef7cbf56c52a87c102a649f9a94cc7d937f4
Moh. Al-Fadhel D. Cali <mdcali@up.edu.ph>
2022-05-24 12:00:52 +0800

    Update settings.py

--- daqs/settings.py
+++ daqs/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'daqsDB',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'thegoldenchild',
